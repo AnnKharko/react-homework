a2adf64b018ff58f44e41ed28834aba4fc914c15
Ann <kharko2112.mechmat@gmail.com>
2020-12-29 13:24:50 +0200

    useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React, {Component} from 'react';
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-              
-            </div>
-        );
+import React, {memo, useMemo,useState, useEffect, useReducer} from "react";
+
+export default function App() {
+    const [counter, SetCounter] = useState(0);
+    const onclickHandler = () => {
+        // SetCounter(counter + 1);
+        SetCounter((prevState) => prevState + 1)
     }
+    const decHandler = () => {
+        //SetCounter(counter-1);
+        SetCounter((prevState) => prevState - 1)
+    }
+    const resetHandler = () => {
+        SetCounter(0);
+    }
+    return (
+        <div>
+            <button onClick={onclickHandler}>Inc</button>
+            <button onClick={decHandler}>Dec</button>
+            <button onClick={resetHandler}>Reset</button>
+            <h1> Counter value: {counter}</h1>
+            <h2> Start editing to see some magic happen!</h2>
+        </div>
+    )
 }
-
-export default App;
