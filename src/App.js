2676706e0cecb6381835b71b1e0e77b353b03729
Ann <kharko2112.mechmat@gmail.com>
2020-12-29 14:17:57 +0200

    useEffect

--- src/App.js
+++ src/App.js
@@ -2,24 +2,35 @@
 import React, {memo, useMemo,useState, useEffect, useReducer} from "react";
 
 export default function App() {
-    const [counter, SetCounter] = useState(0);
+    const [user, SetUser] = useState({name: 'Anna', age: 20});
+    useEffect(() => {
+        console.log('i was called');
+    },[user.name]);
     const onclickHandler = () => {
-        // SetCounter(counter + 1);
-        SetCounter((prevState) => prevState + 1)
+        SetUser((prevState) => ({...prevState,
+        age: prevState.age + 1}));
     }
-    const decHandler = () => {
-        //SetCounter(counter-1);
-        SetCounter((prevState) => prevState - 1)
+    const decHandler= () => {
+        SetUser((prevState) => ({...prevState,
+            age: prevState.age - 1}));
     }
     const resetHandler = () => {
-        SetCounter(0);
+        SetUser((prevState) => ({...prevState,
+            age: 0}));
     }
+    const userNameClickHandler = () => {
+        SetUser((prevState) => ({...prevState,
+        name: prevState.name + '!'}))
+    }
+
     return (
         <div>
             <button onClick={onclickHandler}>Inc</button>
             <button onClick={decHandler}>Dec</button>
             <button onClick={resetHandler}>Reset</button>
-            <h1> Counter value: {counter}</h1>
+            <button onClick={userNameClickHandler}>change user name</button>
+            <h1> User name: {user.name}</h1>
+            <h1> User age: {user.age}</h1>
             <h2> Start editing to see some magic happen!</h2>
         </div>
     )
