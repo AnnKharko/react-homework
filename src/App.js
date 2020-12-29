4ecc43cc49d90965ad6589a287008522a37f381d
Ann <kharko2112.mechmat@gmail.com>
2020-12-29 16:21:28 +0200

    useEffect with fetch

--- src/App.js
+++ src/App.js
@@ -3,20 +3,22 @@ import React, {memo, useMemo,useState, useEffect, useReducer} from "react";
 
 export default function App() {
     const [user, SetUser] = useState({name: 'Anna', age: 20});
+    const [counter, SetCounter] = useState(1);
+    const [todo, SetTodo] = useState();
+
     useEffect(() => {
         console.log('i was called');
-    },[user.name]);
-    const onclickHandler = () => {
-        SetUser((prevState) => ({...prevState,
-        age: prevState.age + 1}));
-    }
+        fetch(`https://jsonplaceholder.typicode.com/todos/${counter}`)
+            .then( value => value.json())
+            .then(json => SetTodo(json));
+    },[counter]);
+    const onclickHandler = () => SetCounter((prev) => prev + 1);
+
     const decHandler= () => {
-        SetUser((prevState) => ({...prevState,
-            age: prevState.age - 1}));
+        SetCounter((prev) => prev - 1);
     }
     const resetHandler = () => {
-        SetUser((prevState) => ({...prevState,
-            age: 0}));
+        SetCounter((prev) => 0 );
     }
     const userNameClickHandler = () => {
         SetUser((prevState) => ({...prevState,
@@ -29,9 +31,15 @@ export default function App() {
             <button onClick={decHandler}>Dec</button>
             <button onClick={resetHandler}>Reset</button>
             <button onClick={userNameClickHandler}>change user name</button>
-            <h1> User name: {user.name}</h1>
-            <h1> User age: {user.age}</h1>
-            <h2> Start editing to see some magic happen!</h2>
+            <h1>Counter value: {counter}</h1>
+            { !!todo && (
+                <>
+                <h2> {todo.id}</h2>
+                <h2> {todo.title}</h2>
+                <h2> {todo.completed.toString()}</h2>
+                <h2> Start editing to see some magic happen!</h2>
+                </>
+                )}
         </div>
     )
 }
