80c72b6eda1c52ca2071c1b926aea29edf0a2e40
Ann <kharko2112.mechmat@gmail.com>
2021-01-03 20:50:39 +0200

    redux-part1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React, {Component} from 'react';
+import React from 'react';
+import {useSelector, useDispatch} from "react-redux";
+import {INC_COUNTER,
+    DEC_COUNTER,
+    RESET_COUNTER} from './redux/action-types';
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-              
-            </div>
-        );
-    }
+export default function App() {
+    const counter = useSelector(({counter}) => counter);
+    const dispatch = useDispatch();
+    const handleInc = () => dispatch({type: INC_COUNTER});
+    const handleDec = () => dispatch({type: DEC_COUNTER});
+    const handleReset = () => dispatch({type: RESET_COUNTER});
+
+    return (
+        <div>
+            <h1> Hello CodeSandBox {counter}</h1>
+            <button onClick={handleInc}>Inc</button>
+            <button onClick={handleDec}>Dec</button>
+            <button onClick={handleReset}>Reset</button>
+        </div>
+    )
 }
-
-export default App;
